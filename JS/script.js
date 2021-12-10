0057ec6c003552c9e705e355f59a0ac7c6c6d458
Faith Alandi <faefaith@gmail.com>
2021-12-10 13:16:22 -0800

    second edit of files

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,14 @@
-
-
+let contactusinfo = []
 function createdarkMode(togglebutton) {
     togglebutton = document.getElementsByClassName("toggle").addEventListener("click", e=>){
         element.classList.toggle("dark-mode");
     }
 }
 
+function contactsubmit(submitbutton)
+    submitbutton = document.getElementById("frm-btm").addEventListener("click", e=>){
+        for (let i = 0; i < contactusinfo.length; i++)
+        
+
+        }
+    }
