4e5c2513825b4f1c83daf8a5be455e4af426fbab
Faith Alandi <faefaith@gmail.com>
2021-12-10 13:27:14 -0800

    third set of changes to files

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,12 @@
-let contactusinfo = []
-function createdarkMode(togglebutton) {
+
+function createdarkMode() {
     togglebutton = document.getElementsByClassName("toggle").addEventListener("click", e=>){
-        element.classList.toggle("dark-mode");
+        document.getElementsByClassName("light-theme").classList.remove("light-theme")
+        document.getElementsByClassName("dark-mode").classList.add("dark-mode")
+
     }
 }
 
 function contactsubmit(submitbutton)
     submitbutton = document.getElementById("frm-btm").addEventListener("click", e=>){
         for (let i = 0; i < contactusinfo.length; i++)
-        
-
-        }
-    }
